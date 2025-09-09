d936ce5111339c6db7161248b3a9af008aaad905
SafnasKaldeen <safnaskm.20@uom.lk>
2025-09-09 13:24:39 +0530

    /redirect fixed

--- lib/auth-config.ts
+++ lib/auth-config.ts
@@ -11,7 +11,7 @@ export const authConfig = {
   oauth: {
     domain: process.env.NEXT_PUBLIC_COGNITO_DOMAIN || "",
     scope: ["email", "openid", "profile"],
-    redirectSignIn: process.env.NEXT_PUBLIC_REDIRECT_SIGN_IN || "http://localhost:3000/dashboard",
+    redirectSignIn: process.env.NEXT_PUBLIC_REDIRECT_SIGN_IN || "http://localhost:3000/",
     redirectSignOut: process.env.NEXT_PUBLIC_REDIRECT_SIGN_OUT || "http://localhost:3000/landing",
     responseType: "code" as const,
   },
